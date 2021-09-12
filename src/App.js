ebafa0a573f97affbe98d0435568681a0f51fc91
unknown <Bizcanna21@icloud.com>
2021-09-12 18:10:13 -0500

    updated display name data list table components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import DataTable from './DataTable';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Person FirstName and lastName</h1>
+state = {
+    firstName: 'Reggie',
+    lastName: 'White'
+}
+state = {
+    names: ['Mike', 'Nevin', 'Aaron', 'Tory', 'Kelly']
+}
+
+<button onClick={DevCodeCamp}>Click</button>
+
     </div>
   );
 }
+state = {
+  superheroes = [
+      {
+          superheroId: 1,
+          name: 'Batman',
+          primaryAbility: 'Wealthy',
+          secondarAbility: 'Rich'
+      },
+      {
+          superheroId: 2,
+          name: 'Superman',
+          primaryAbility: 'Super strength',
+          secondarAbility: 'Fly'
+      },
+      {
+          superheroId: 3,
+          name: 'Spiderman',
+          primaryAbility: 'Spider senses',
+          secondarAbility: 'Shoots web'
+      }
+  ]
+}
+
+
 
 export default App;
