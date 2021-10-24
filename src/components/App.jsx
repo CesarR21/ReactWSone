74ddd678a95abbfc479522e1e9214b786f03ed6d
unknown <Bizcanna21@icloud.com>
2021-10-23 20:05:09 -0500

    3 complete ex

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,119 +1,58 @@
-import React from "react";
-import React, { Component } from "react";
-import './App.css';
-import DataTable from './DataTable';
+import React, { Component } from 'react';
+import DisplayName from './DisplayName/DisplayName';
+import NamesList from './NamesList/NamesList';
+import AlertUser from './AlertUser/AlertUser';
 
 
 class App extends Component {
-    State =  { 
-        value(props)
-    }
-const DisplayName = props => {
-    return (
-      <p>Reggie White</p>
-  )
-}
-MyComponent.displayName = "MyComponent"
-    <ul>
-       {(list || [this.state]).map(item => (
-          <ListItem key={state.id} item={state} />
- ))}</ul>
+    constructor(props){
+        super(props);
+        this.names = [
+            'Mike', 'John', 'Travis', 'Akeem'
+        ];
 
- render(){
+        this.super = [
+            {
+                superheroId: 1,
+                name: 'Batman',
+                primaryAbility: 'Wealthy',
+                secondarAbility: 'Rich'
+            },
+            {
+                superheroId: 2,
+                name: 'Superman',
+                primaryAbility: 'Super strength',
+                secondarAbility: 'Fly'
+            },
+            {
+                superheroId: 3,
+                name: 'Spiderman',
+                primaryAbility: 'Spider senses',
+                secondarAbility: 'Shoots web'
+            }
+        ]
+
+        this.state = {
+            firstName: 'Reggie',
+            lastName: 'White',
+        };
+    }
+
+    display(message){
+        alert(message);
+    }
+
+    render(){
         return (
-            <h1>Reggie White</h1>
+            <div className="container-fluid">
+                <DisplayName firstName={this.state.firstName} lastName={this.state.lastName}/>
+                <NamesList names={this.names}/>
+                <AlertUser 
+                    buttonClick={(message) => this.display(message)}
+                />
+            </div>
         )
     }
 }
-  class App extends Component
-  
-
-render(){
-     const { state } = this.state.props
-     const namesList = state.map(name => {
-       consle.log(namesList)
-     })
-     return(
-       <ul>
-          <li personName="Mike"></li>
-          <li personName="Nevin"></li>
-          <li personName="Aaron"></li>
-          <li personName="Tory"></li>
-          <li personName="Kelly"></li>
-          </ul>
-      );
-    }
-  }
-
-  class App extends Component {
-  
-    constructor(props) {
-      super(props);
-      this.clickMe = this.clickMe.bind(this);
-    }
-  
-    clickMe() {
-      alert('DevCodeCamp');
-    }
-    
-    return (
-      <button onClick={this.clickMe}>
-        Click me!
-      </button>
-    );
-  }
-
-  class App extends Component {
-    render() {
-  
-      const headings = [
-          'superheroId',
-          'name' ,
-          'primaryAbility',
-          'secondarAbility'
-       ];
-       const rows = [
-        [
-          '1',
-          'BatMan',
-          'Wealthy',
-          'Rich'
-        ],
-        [
-
-          '2',
-          'Superman',
-          'Super strength',
-          'Fly'
-        ],
-        [
-
-          '3',
-          'Spiderman',
-          'Spider senses',
-          'Shoots web'
-        ],
-          
-       return (
-            <AppProvider>
-              <Page title="SuperHeroes">
-                <DataTable headings={headings} rows={rows} />
-              </Page>
-            </AppProvider>
-          );
-        }
-      }
-
-
-
 
 export default App;
-
-        
-
-
-
-
-
-
-
